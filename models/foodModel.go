0a907b4f300f137cc9ea938a09fc0d4ffc6f7c35
Micah <micahshallom@gmail.com>
2023-11-04 13:48:11 +0100

    setup food model struct

--- models/foodModel.go
+++ models/foodModel.go
@@ -1,1 +1,18 @@
 package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Food struct {
+	ID 				primitive.ObjectID			`bson:"_id"`
+	Name 			*string						`json:"name" validate:"required,min=2,max=100"`
+	Price 			*string						`json:"price" validate:"required,min=2,max=100"`
+	FoodImage		*string						`json:"food_image" validate:"required"`
+	CreatedAt		time.Time					`json:"created_at"`
+	UpdatedAt		time.Time					`json:"updated_at"`
+	FoodID			string						`json:"food_id"`
+	MenuID			*string						`json:"menu_id"`
+}
