a5344721fbd3ee6c4dfdd61bf93b87cc9ab13dfd
Micah <micahshallom@gmail.com>
2023-11-04 13:54:30 +0100

    setup invoice model struct

---
+++ models/invoiceModel.go
@@ -0,0 +1,17 @@
+package models
+
+import (
+	"time"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Invoice struct {
+	ID 						primitive.ObjectID				`bson:"_id"`
+	InvoiceID				string							`json:"invoice_id"`
+	OrderID					string							`json:"order_id"`
+	PaymentMethod			*string							`json:"payment_method" validate:"eq=CARD|eq=CASH|eq="`
+	PaymentStatus			*string							`json:"payment_status" validate:"required,eq=PENDING|eq=PAID"`
+	PaymentDueDate			time.Time						`json:"payment_due_date"`
+	CreatedAt				time.Time						`json:"created_at"`
+	UpdatedAt				time.Time						`json:"updated_at"`
+}		
